15484d81059d8e4d35c0914e120c5eb3d008133a
Michael Boroski <mjboroski@gmail.com>
2018-07-07 19:44:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 let currentUser = 'me';
 let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
 let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = 'Welcome, ' + currentUser + '!';
